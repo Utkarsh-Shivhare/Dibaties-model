a8f83780feae6765f955d963f716c4084e15a2e7
Utkarsh-Shivhare <109977467+Utkarsh-Shivhare@users.noreply.github.com>
2022-12-20 12:20:10 +0530

    Update index.py

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ import streamlit as st
 
 
 # loading the saved model
-loaded_model = pickle.load(open('model.pkl', 'rb'))
+loaded_model = pickle.load(open('model.sav', 'rb'))
 
 
 # creating a function for Prediction
